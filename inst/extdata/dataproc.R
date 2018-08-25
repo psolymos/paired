d9948bc28d468b7be1d4f2f9cf60c5ec220f5ae6
Peter Solymos <psolymos@gmail.com>
2018-08-24 22:19:31 -0600

    some cleanup was necessary

--- inst/extdata/dataproc.R
+++ inst/extdata/dataproc.R
@@ -1,3 +1,13 @@
-paired <- read.csv("~/repos/paired/inst/extdata/aru-join-table_2016-AUG-22.csv")
-str(paired)
+x <- read.csv("~/repos/paired/inst/extdata/aru-join-table_2016-AUG-22.csv")
+str(x)
+
+levels(x$TimeInterval)[levels(x$TimeInterval) == ""] <- "UNK"
+levels(x$Interval)[levels(x$Interval) == "0-1 min"] <- "0-3 min"
+levels(x$Interval)[levels(x$Interval) == "2-3 min"] <- "0-3 min"
+levels(x$Interval)[levels(x$Interval) == "8-9 min"] <- "5-10 min"
+table(x$TimeInterval,x$Interval)
+
+x <- droplevels(x)
+paired <- x
+
 save(paired, file="~/repos/paired/data/paired.rda")
